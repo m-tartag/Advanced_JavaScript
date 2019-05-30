435a79f84a31d954ded79c8ba01fe35d33ddb7a0
m-tartag <mattbrs51@gmail.com>
2019-05-29 23:35:34 -0400

    All DONE

--- jsAssignment/script.js
+++ jsAssignment/script.js
@@ -19,22 +19,55 @@ const numbers = [
 
 // 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
 // Using a for loop:
+
+(function ForLoop() {
+  const result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    result.push(String.fromCharCode(numbers[i]));
+  }
+
+  document.getElementById('oneA').innerHTML = result;
+})();
+
 // Using the forEach method:
+
+(function ForEachLoop() {
+  const result = [];
+  numbers.forEach(function(num) {
+    result.push(String.fromCharCode(num));
+  });
+  document.getElementById('oneB').innerHTML = result;
+})();
+
 // Using the map method:
+
+(function Mapper() {
+  const result = [];
+  numbers.map(function(num) {
+    result.push(String.fromCharCode(num));
+  });
+
+  document.getElementById('oneC').innerHTML = result;
+})();
 
 // 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
 // Answer:
 
-const Filterer = numbers.filter(function(num) {
-  return num > 72 && num < 88;
-});
+(function Filterer() {
+  document.getElementById('two').innerHTML = numbers.filter(function(num) {
+    return num > 72 && num < 88;
+  });
+})();
 
-console.log('Filterer :', Filterer);
 // 3. Display the product of all numbers using reduce
 // Answer:
 
-const Reducer = numbers.reduce(function(acc, total) {
-  return acc + total;
-});
-
-console.log('Reducer :', Reducer);
+(function Reducer() {
+  document.getElementById('three').innerHTML = numbers.reduce(function(
+    acc,
+    total
+  ) {
+    return acc + total;
+  });
+})();
