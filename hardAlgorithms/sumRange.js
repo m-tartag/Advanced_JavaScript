712c618b45dba0a0e4c86cfe90c8b44a703de0d4
m-tartag <mattbrs51@gmail.com>
2019-06-06 11:54:52 -0400

    completed sumRange algorithm!

---
+++ hardAlgorithms/sumRange.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+  const ordered = arr.sort((a, b) => a - b);
+  let storage = 0;
+
+  for (let i = ordered[0]; i <= ordered[ordered.length - 1]; i++) {
+    storage += i;
+  }
+  console.log(storage);
+}
+
+sumAll([1, 4, 12]);
