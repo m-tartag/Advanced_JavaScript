4eccbf96b6fc3992be31564a9c55fa52cfbe8f1d
m-tartag <mattbrs51@gmail.com>
2019-06-08 13:02:30 -0400

    working on algos

--- hardAlgorithms/sumPrimes.js
+++ hardAlgorithms/sumPrimes.js
@@ -1,5 +1,24 @@
 function sumPrimes(num) {
-  return num;
+  const prime = [];
+  const notPrime = [];
+
+  for (let i = 2; i <= num; i++) {
+    // 2 is prime
+    if (i === 2) {
+      prime.push(i);
+      // prime numbers are odd SO all evens are not prime
+    } else if (i % 2 === 0) {
+      notPrime.push(i);
+      // logic for determining whether or not ODD numbers are prime
+    } else {
+      for (let j = 1; j <= i; j++) {
+        console.log(i / j);
+      }
+    }
+  }
+
+  console.log(notPrime);
+  console.log(prime);
 }
 
-sumPrimes(10);
+sumPrimes(5);
