9c8eac8a7143b1eb7ca0487fe9988b022177ab96
m-tartag <mattbrs51@gmail.com>
2019-08-20 00:34:49 -0400

    practice

--- review/async.js
+++ review/async.js
@@ -4,3 +4,11 @@ const fruits = ['Apple', 'Banana', 'Orange', 'Strawberry'];
 fruits.forEach(function(val) {
   console.log(`FRONT${val}BACK`);
 });
+
+// SAME AS THIS
+
+function callback(val) {
+  console.log(`FRONT${val}BACK`);
+}
+
+fruits.forEach(callback);
